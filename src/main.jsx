f90605c79019089b1138964c847bff89f9a6eaf0
GouravSaini2004 <mrgoravsainimrt@gmail.com>
2024-10-14 11:58:27 +0530

    hash route

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
-import ProtectedRoute from './protected.js'
-import Home from './pages/home.jsx'
-import Login from './pages/login.jsx'
-import Register from './pages/register.jsx'
-import CompetitionForm from './pages/apply.jsx'
-import  AddGame from './pages/addgame.jsx'
-import GameTable from './component/table.jsx'
-import UserProfile from './pages/profile.jsx'
-import ContactUs from './pages/contectus.jsx'
-import TermsAndConditions from './pages/terms.jsx'
-import AboutUs from './pages/aboutus.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import App from './App.jsx';
+import Home from './pages/home.jsx';
+import Login from './pages/login.jsx';
+import Register from './pages/register.jsx';
+import CompetitionForm from './pages/apply.jsx';
+import AddGame from './pages/addgame.jsx';
+import GameTable from './component/table.jsx';
+import UserProfile from './pages/profile.jsx';
+import ContactUs from './pages/contectus.jsx';
+import TermsAndConditions from './pages/terms.jsx';
+import AboutUs from './pages/aboutus.jsx';
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/contactUs' element={<ContactUs />} />
-      <Route path='/aboutUs' element={<AboutUs />} />
-      <Route path='/termsAndConditions' element={<TermsAndConditions />} />
-      {/* <Route path='/profile' element={<ProtectedRoute element={<UserProfile />} />} /> */}
-      <Route path='/addgame'  element={<ProtectedRoute element={<AddGame />} />} />
-      <Route path='/apply/:id' element={<ProtectedRoute element={<CompetitionForm />} />} />
-      <Route path='/table' element={<ProtectedRoute element={<GameTable />} />}/>
-      <Route path='/profile' element={<ProtectedRoute  element={<UserProfile />} />} />
-      
-    </Route>
-  )
-)
-
+function Main() {
+  return (
+    <HashRouter>
+      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/termsAndConditions" element={<TermsAndConditions />} />
+        <Route path="/addgame" element={<AddGame />} />
+        <Route path="/apply/:id" element={<CompetitionForm />} />
+        <Route path="/table" element={<GameTable />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+    </HashRouter>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <Main />
+  </StrictMode>
+);
